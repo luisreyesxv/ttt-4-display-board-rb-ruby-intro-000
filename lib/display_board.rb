ee1e26e8ede1c62c0fc748ec578617661c72a9e0
Luis Reyes <luisreyesxv@yahoo.com>
2020-04-25 21:56:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 def display_board(position)
 
- puts " #{position[0]} " +"|"+" #{position[1]} "+"|"+" #{position[2] }"
+ puts " #{position[0]} " +"|"+" #{position[1]} "+"|"+" #{position[2] } "
  puts "-----------"
- puts " #{position[3]} " +"|"+" #{position[4]} "+"|"+" #{position[5] }"
+ puts " #{position[3]} " +"|"+" #{position[4]} "+"|"+" #{position[5] } "
  puts "-----------"
- puts " #{position[6]} " +"|"+" #{position[7]} "+"|"+" #{position[8] }"
+ puts " #{position[6]} " +"|"+" #{position[7]} "+"|"+" #{position[8] } "
 end
